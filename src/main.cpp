323ce10b06bf2ee7a36cca78a8dc98af5e827950
geobrgdps <geobrgdps@gmail.com>
2026-07-22 13:16:03 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,20 +7,12 @@ class $modify(GeoBRMenuLayer, MenuLayer) {
     bool init() {
         if (!MenuLayer::init()) return false;
 
-        // Procura pelo menu de botões da tela inicial
         auto bottomMenu = this->getChildByID("bottom-menu");
         if (!bottomMenu) {
             bottomMenu = this->getChildByID("main-menu");
         }
 
-        // Criar o botão da GeoBR
-        auto spr = CircleButtonSprite::createWithSpriteFrameName(
-            "geode.loader/geode-logo.png", // Ícone temporário do Geode
-            0.85f,
-            CircleBaseColor::Green,
-            CircleBaseSize::Medium
-        );
-
+        auto spr = ButtonSprite::create("GeoBR", "goldFont.fnt", "GJ_button_01.png", .8f);
         auto btn = CCMenuItemSpriteExtra::create(
             spr,
             this,
@@ -37,18 +29,13 @@ class $modify(GeoBRMenuLayer, MenuLayer) {
     }
 
     void onGeoBRBtn(CCObject*) {
-        // Pop-up ao clicar no botão
         createQuickPopup(
             "GeoBR GDPS",
-            "Bem-vindo ao portal in-game da <cy>GeoBR</c>!\nEscolha uma das opcoes abaixo:",
-            "Discord", "Demon List",
+            "Bem-vindo ao portal in-game da <cy>GeoBR</c>!",
+            "Fechar", "Demon List",
             [](auto, bool btn2) {
                 if (btn2) {
-                    // Abre o site da Demon List que criamos
-                    web::openHTML("https://luizcxoinavile-hash.github.io/Testes/");
-                } else {
-                    // Abre o Discord da GeoBR
-                    web::openHTML("https://discord.gg/sdYx74wG9");
+                    web::openHTML("https://geobrgdps.github.io/Testes/");
                 }
             }
         );
